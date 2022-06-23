3f2f3377bc8c970e64299f88a4754b0621d375a6
Ali Al-Dhib <alifahed67@gmail.com>
2022-06-23 22:14:42 +0800

    Declare round winner

--- script.js
+++ script.js
@@ -26,6 +26,42 @@ function computerSelection() {
 
 function playRound(playerSelection, computerSelection) {
   console.log(`Player selected ${playerSelection} while computer selected ${computerSelection}.`);
+  let roundResult;
+  if (playerSelection === computerSelection) {
+    roundResult = "Tie";
+    console.log(roundResult);
+    return roundResult;
+  }
+
+  // odds of player winning the round
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    roundResult = "You Win! Rock beats Paper";
+    console.log(roundResult);
+    return roundResult;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    roundResult = "You Win! Scissors beats Paper";
+    console.log(roundResult);
+    return roundResult;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    roundResult = "You Win! Paper beats Rock";
+    console.log(roundResult);
+    return roundResult;
+  }
+
+  // odds of computer winning the round
+  if (computerSelection === "rock" && playerSelection === "scissors") {
+    roundResult = "You Lose! Rock beats Paper";
+    console.log(roundResult);
+    return roundResult;
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+    roundResult = "You Lose! Scissors beats Paper";
+    console.log(roundResult);
+    return roundResult;
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    roundResult = "You Lose! Paper beats Rock";
+    console.log(roundResult);
+    return roundResult;
+  }
 }
 
 playRound(playerSelection(), computerSelection());
