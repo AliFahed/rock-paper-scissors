0b76051d5624f0317dbed7639ae2532cb89aaff4
Ali Al-Dhib <alifahed67@gmail.com>
2022-06-25 00:53:23 +0800

    Add announce winner function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerWins = 0;
+let computerWins = 0;
+
 function playerSelection() {
   let playerSelection = prompt("Choose 'rock', 'paper', or 'scissors'");
   // player input should be case insensitive
@@ -29,6 +32,8 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     let roundResult;
     roundResult = "Tie";
+    playerWins += 1;
+    computerWins += 1;
 
     console.log(roundResult);
     return roundResult;
@@ -42,26 +47,48 @@ function checkRoundWinner(playerSelection, computerSelection) {
 
   if (playerSelection === "rock" && computerSelection === "scissors") {
     roundResult = "You Win! Rock beats Scissors";
+    playerWins += 1;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     roundResult = "You Win! Paper beats Rock";
+    playerWins += 1;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     roundResult = "You Win! Scissors beats Rock";
+    playerWins += 1;
   } else if (computerSelection === "rock" && playerSelection === "scissors") {
     roundResult = "You Lose! Rock beats Scissors";
+    computerWins += 1;
   } else if (computerSelection === "paper" && playerSelection === "rock") {
     roundResult = "You Lose! Paper beats Rock";
+    computerWins += 1;
   } else if (computerSelection === "scissors" && playerSelection === "paper") {
     roundResult = "You Lose! Scissors beats Paper";
+    computerWins += 1;
   }
 
   console.log(roundResult);
   return roundResult;
 }
 
+function announceWinner() {
+  let allRoudnsWinner;
+  if (playerWins === computerWins) {
+    allRoudnsWinner = "It's a Tie!";
+  } else if (playerWins > computerWins) {
+    allRoudnsWinner = "You Won the Game!";
+  } else {
+    allRoudnsWinner = "You Lost the Game!"
+  }
+
+  console.log(allRoudnsWinner);
+  return allRoudnsWinner;
+}
+
 function game() {
   for (let i = 0; i < 5; i++) {
     playRound(playerSelection(), computerSelection());
   }
+  
+  announceWinner();
 }
 
 game();
